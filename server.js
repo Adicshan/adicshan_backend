2a770618b824c3813180ba6140a4f287f3cbaa22
Adicshan <129241416+Adicshan@users.noreply.github.com>
2023-08-23 11:15:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,39 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.post('/tennis', (req, res) => {
-  // Handle the request here
-  const {outlook,temperature, humidity,windy}= req.body;
-  
-  console.log(outlook);
-  
-  const pythonScriptPath = path.join(__dirname, '..', 'server', 'scripts', 'logisticRegression.py');
-  const scriptDirectory = path.dirname(pythonScriptPath);
-  
-  const pythonProcess = spawn('python', [pythonScriptPath, outlook, temperature, humidity, windy], {
-      cwd: scriptDirectory
-  });
-  
 
-
-pythonProcess.stdout.on('data',(data)=>{
-  const output =data.toString();
-  
-  console.log('python output:',output);
-  res.status(200).json({output:output})
-});
-
-pythonProcess.stderr.on('data',(data)=>{
-  const errors = data.toString();
-  console.log('python error:',errors);
-});
-pythonProcess.on('close', (code) => {
-  console.log(`Python process exited with code ${code}`);
-
- 
-});
-
-});
 
 app.post('/exam', (req, res) => {
   const { toughness, hour, consist, syllabus, time } = req.body;
@@ -54,11 +22,9 @@ app.post('/exam', (req, res) => {
 
 console.log('Toughness:', toughness);
 const pythonScriptPath = path.join(__dirname, 'scripts', 'logisticRegression.py');
-  const scriptDirectory = path.dirname(pythonScriptPath);
+
   
-  const pythonProcess = spawn('python', [pythonScriptPath, outlook, temperature, humidity, windy], {
-      cwd: scriptDirectory
-  });
+  const pythonProcess = spawn('python', [pythonScriptPath, outlook, temperature, humidity, windy]);
   
 
 ExamProcess.stdout.on('data',(data)=>{
@@ -78,6 +44,6 @@ ExamProcess.on('close',(code)=>{
 
 
 app.listen(port, () => {
-  console.log(`Server is rng on port ${port}`);
+  console.log(`Server is raan on port ${port}`);
   console.log(`${url}`);
 });
