33d8bd0a0541b8ce3141b032af8df366c8f47ef7
Adicshan <129241416+Adicshan@users.noreply.github.com>
2023-08-24 22:11:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ ExamProcess.on('close',(code)=>{
 
 
 app.listen(port, () => {
-  console.log(`Server is riooooooonng on port ${port}`);
+  console.log(`Server is ripppppppppnng on port ${port}`);
   console.log(`${url}`);
 });
