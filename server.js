bb2e1e152f094b6739c4e2d185af718fc5f375d0
Adicshan <129241416+Adicshan@users.noreply.github.com>
2023-08-26 20:40:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ const cors=require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors(
-  {
-    origin:["https://deploy-mern-lwhq.vercel.app"],
-    methods:["POST","GET"],
-    credentials: true
-  }
-));
+app.use(cors());
 
 
 
